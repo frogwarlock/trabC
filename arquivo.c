cdbd7c99f588b5e854e4ac6894f5ee934a6603f9
frogwarlock <93387620+frogwarlock@users.noreply.github.com>
2023-09-14 13:02:18 -0300

    início arquivo

--- arquivo.c
+++ arquivo.c
@@ -1,10 +1,31 @@
- #include <stdio.h>
+#include <stdio.h>
 
-void lalalla(){
+// ler arquivo txt
+// realizar operações necessárias (funcoes de leitura)
+// colocar output em novo txt
 
-    printf("TESTE TESTE");
-}
+// o que as funções fazem:
+// imprime o log de vendas
+// imprime catálogo de produtos 
+// imprime lista de vendedores 
+// mostra o total vendido
+// mostra o total vendido por produto ex(p 35(prato): R$ 75.00)
+// mostra o total vendido por vendedor ex.(v 54(Marisa): R$170.00)
+
+
+int matriz_produtos[3][3] = {
+    {35, 25.00, 'Prato'},
+    {60, 10.00, 'Garfo'},
+    {89, 20.00, 'Faca'}
+  };
+
+int matriz_vendedores[2][2] = {
+    {54, 'Marisa'},
+    {71, 'Elis'},
+    {88, 'Elza'}
+};
 
 void main(){
-    lalalla();
+  
+
 }
