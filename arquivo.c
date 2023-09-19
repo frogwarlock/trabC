4723ed7dacfbcde7405488759a5c591d842dce0d
frogwarlock <93387620+frogwarlock@users.noreply.github.com>
2023-09-18 21:05:29 -0300

    aplicação de funções
    deu bom, tem um bug mas eu to com medo de mexer

--- arquivo.c
+++ arquivo.c
@@ -42,85 +42,35 @@ typedef struct
     int unidades;
 } Venda;
 
-
-
+//chama func antes da main
+void lerProduto(Produto produtos[], int *num_produtos);
+void lerVendedores(Vendedor vendedores[], int *num_vendedor);
+void lerVendas(Venda vendas[], int *num_vendas);
+void calculaTotalVendedores(Venda vendas[], Produto produtos[], Vendedor vendedores[], int num_vendas, int num_produtos, int num_vendedores, float totalVendasxVendedores[]);
+void calculaTotalProdutos(Venda vendas[], Produto produtos[], int num_vendas, int num_produtos, float totalVendasxProdutos[]);
+void fazArquivosTotais(float totalGeral, Produto produtos[], Vendedor vendedores[], float totalVendasxVendedores[], float totalVendasxProdutos[], int num_produtos, int num_vendedor);
 
 int main()
 {
     Produto produtos[max_produtos];
     int num_produtos = 0;
 
-    FILE *produtos_arq = fopen("produtos.txt", "r");
-    if (produtos_arq == NULL)
-    {
-        printf("Um erro ocorreu ao abrir o arquivo produtos");
-        return 1; 
-    }
-
-    while (fscanf(produtos_arq, "%d %f %[^\n]", &produtos[num_produtos].codigo, &produtos[num_produtos].preco, produtos[num_produtos].descricao) != EOF)
-    {
-        num_produtos++;
-    }
-
-    fclose(produtos_arq);
-
     Vendedor vendedores[max_vendedores];
     int num_vendedor = 0;
-
-    FILE *vendedor_arq = fopen("vendedores.txt", "r");
-    if (vendedor_arq == NULL)
-    {
-        printf("Um erro ocorreu ao abrir o arquivo vendedores ");
-        return 1; 
-    }
-
-    while (fscanf(vendedor_arq, "%d %[^\n]", &vendedores[num_vendedor].codigo, vendedores[num_vendedor].nome) != EOF) // END OF FILE
-    {
-        num_vendedor++;
-    }
-
-    fclose(vendedor_arq);
 
     Venda vendas[max_vendas];
     int num_vendas = 0;
 
-    FILE *vendas_arq = fopen("vendas.txt", "r");
-    if (vendas_arq == NULL)
-    {
-        printf("Um erro ocorreu ao abrir o arquivo vendas");
-        return 1; 
-    }
+    lerProduto(produtos, &num_produtos);
+    lerVendedores(vendedores, &num_vendedor);
+    lerVendas(vendas, &num_vendas);
 
-    while (fscanf(vendas_arq, "%d %d %d", &vendas[num_vendas].codigo_vendedor, &vendas[num_vendas].codigo_produto, &vendas[num_vendas].unidades) != EOF)
-    {
-        num_vendas++;
-    }
-
-    fclose(vendas_arq);
-
-    // Calc de vendas
-
-    // Total de vendas dos vendedores
+    // Calcula os totais de vendas
     float totalVendasxVendedores[max_vendedores] = {0};
+    calculaTotalVendedores(vendas, produtos, vendedores, num_vendas, num_produtos, num_vendedor, totalVendasxVendedores);
 
-    for (int i = 0; i < num_vendas; i++)
-    {
-        int codigo_vendedor = vendas[i].codigo_vendedor - 1;
-        int codigo_produto = vendas[i].codigo_produto - 1;
-        float preco = produtos[codigo_produto].preco;
-        int unidades = vendas[i].unidades;
-        totalVendasxVendedores[codigo_vendedor] += preco * unidades;
-    }
-
-    // Total de vendas por produtos
     float totalVendasxProdutos[max_produtos] = {0};
-    for (int i = 0; i < num_vendas; i++)
-    {
-        int codigo_produto = vendas[i].codigo_produto - 1;
-        float preco = produtos[codigo_produto].preco;
-        int unidades = vendas[i].unidades;
-        totalVendasxProdutos[codigo_produto] += preco * unidades;
-    }
+    calculaTotalProdutos(vendas, produtos, num_vendas, num_produtos, totalVendasxProdutos);
 
     float totalGeral = 0;
 
@@ -129,12 +79,95 @@ int main()
         totalGeral += totalVendasxProdutos[i];
     }
 
+    fazArquivosTotais(totalGeral, produtos, vendedores, totalVendasxVendedores, totalVendasxProdutos, num_produtos, num_vendedor);
+
+    return 0; 
+}
+
+void lerProduto(Produto produtos[], int *num_produtos)
+{
+    FILE *produtos_arq = fopen("produtos.txt", "r");
+    if (produtos_arq == NULL)
+    {
+        printf("Um erro ocorreu ao abrir o arquivo produtos");
+        return;
+    }
+
+    while (fscanf(produtos_arq, "%d %f %[^\n]", &produtos[*num_produtos].codigo, &produtos[*num_produtos].preco, produtos[*num_produtos].descricao) != EOF)
+    {
+        (*num_produtos)++;
+    }
+
+    fclose(produtos_arq);
+}
+
+void lerVendedores(Vendedor vendedores[], int *num_vendedor)
+{
+    FILE *vendedor_arq = fopen("vendedores.txt", "r");
+    if (vendedor_arq == NULL)
+    {
+        printf("Um erro ocorreu ao abrir o arquivo vendedores ");
+        return;
+    }
+
+    while (fscanf(vendedor_arq, "%d %[^\n]", &vendedores[*num_vendedor].codigo, vendedores[*num_vendedor].nome) != EOF)
+    {
+        (*num_vendedor)++;
+    }
+
+    fclose(vendedor_arq);
+}
+
+void lerVendas(Venda vendas [], int *num_vendas)
+{
+    FILE *vendas_arq = fopen("vendas.txt", "r");
+    if (vendas_arq == NULL)
+    {
+        printf("Um erro ocorreu ao abrir o arquivo vendas");
+        return;
+    }
+
+    while (fscanf(vendas_arq, "%d %d %d", &vendas[*num_vendas].codigo_vendedor, &vendas[*num_vendas].codigo_produto, &vendas[*num_vendas].unidades) != EOF)
+    {
+        (*num_vendas)++;
+    }
+
+    fclose(vendas_arq);
+}
+
+void calculaTotalVendedores(Venda vendas[], Produto produtos[], Vendedor vendedores[], int num_vendas, int num_produtos, int num_vendedores,
+ float totalVendasxVendedores[])
+{
+    for (int i = 0; i < num_vendas; i++)
+    {
+        int codigo_vendedor = vendas[i].codigo_vendedor - 1;
+        int codigo_produto = vendas[i].codigo_produto - 1;
+        float preco = produtos[codigo_produto].preco;
+        int unidades = vendas[i].unidades;
+        totalVendasxVendedores[codigo_vendedor] += preco * unidades;
+    }
+}
+
+void calculaTotalProdutos(Venda vendas[], Produto produtos[], int num_vendas, int num_produtos, float totalVendasxProdutos[])
+{
+    for (int i = 0; i < num_vendas; i++)
+    {
+        int codigo_produto = vendas[i].codigo_produto - 1;
+        float preco = produtos[codigo_produto].preco;
+        int unidades = vendas[i].unidades;
+        totalVendasxProdutos[codigo_produto] += preco * unidades;
+    }
+}
+
+void fazArquivosTotais(float totalGeral, Produto produtos[], Vendedor vendedores[], float totalVendasxVendedores[], float totalVendasxProdutos[],
+ int num_produtos, int num_vendedor)
+{
     // info vendas pro arquivo
     FILE *resultadoVendas = fopen("arquivosTotais.txt", "w");
     if (resultadoVendas == NULL)
     {
         printf("Erro ao criar o arquivo resumoVendas");
-        return 1; 
+        return;
     }
     
     fprintf(resultadoVendas, "TOTAL GERAL VENDIDO: %.2f\n", totalGeral);
@@ -162,7 +195,6 @@ int main()
     fprintf(resultadoVendas, "\n");
 
     fclose(resultadoVendas); 
-
-    return 0; 
 }
+
 
