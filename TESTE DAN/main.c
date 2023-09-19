a5e4c8a75b23af23da8543d4722b0e4953674b25
Daniel <limadanielpereira@gmail.com>
2023-09-19 17:22:11 -0300

    Upload de nova main com comentários
    Nova main com comentários e alteração  no TXT arquivos totais com o append de produtos, vendas e vendedores

--- TESTE DAN/main.c
+++ TESTE DAN/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void ler_arquivo(char* titulo){
+void ler_arquivo(char* titulo){ //Escreve na tela o arquivo com index [0],[1]...
     FILE *arquivo;
     char linha[100];
     char *result;
@@ -22,23 +22,37 @@ void ler_arquivo(char* titulo){
     fclose(arquivo);
 }
 
+void copiarConteudo(FILE *arquivo, FILE *arquivo2){ //Copia um Arquivo e Cola em Outro
+
+    char leitor[100];  // Define o máxima de caractere de uma linha
+
+    while(fgets(leitor, 100, arquivo) != NULL )
+
+
+    fputs(leitor, arquivo2); //Coloca o que tem no arquivo 1 no arquivo 2
+}
+
+
+
 // ALTERAR A MAIN
+
 int main(){
-    int escolha;
-    scanf("%d", &escolha);
-    
-    if (escolha == 1){
-        ler_arquivo("vendas.txt");
-    }
-    else if (escolha == 2){
-        ler_arquivo("produtos.txt");
-    }
-    else if (escolha == 3){
-        ler_arquivo("vendedores.txt");
-    }
-    else{
-        printf("Saindo do modo admin....");
-    }
-    
+    FILE* arquivo_vendedores = fopen("vendedores.txt", "r"); // Abre os 3 txt's com função "read"
+    FILE* arquivo_vendas = fopen("vendas.txt", "r");
+    FILE* arquivo_produtos = fopen("produtos.txt", "r");
+
+    FILE* arquivo_total = fopen("arquivosTotais.txt", "a"); //Abre o arquivo final com função "append"
+
+    ler_arquivo("vendedores.txt");
+    copiarConteudo(arquivo_vendedores, arquivo_total); //Copia o vendedores pro total
+    copiarConteudo(arquivo_vendas, arquivo_total); //Copia a vendas pro total 
+    copiarConteudo(arquivo_produtos, arquivo_total); //Copia produtos pro total
+
+    fclose(arquivo_vendedores); //Fecha todos os arquivos
+    fclose(arquivo_total);
+    fclose(arquivo_vendas);
+    fclose(arquivo_produtos);
+
     return 0;
+
 }
